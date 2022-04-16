2f75d09c1e34511cf5cc4e4ce5f6048908ec104b
NguyenT01 <transinguyen2001@gmail.com>
2022-04-16 09:38:36 +0700

    demo fetchapi

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -16,11 +17,19 @@ app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('/18d?R'));
+app.use(session());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req,res, next)=>{
+  res.locals.flash = req.session.flash
+  delete req.session.flash
+  next()
+})
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.json({code:1, message: 'Fetch API thanh cong :>'})
 });
 
 module.exports = router;
